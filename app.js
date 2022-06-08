9142b0b8ae3d2b8bd0d3c5c3c4119417c0333994
WinsonChen114 <14winsonchen@gmail.com>
2022-06-08 14:51:00 -0400

    Added event listener to form

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 
 const searchText = document.querySelector("#search")
+const formDiv = document.querySelector("#form_div")
 const gifArea = document.querySelector("#gif_area")
+
+formDiv.addEventListener("submit", () => {console.log("Submitted")})
+
